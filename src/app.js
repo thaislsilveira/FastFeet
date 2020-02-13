83ef94f455cc6d939d57d36b732ad79d93d3bcbf
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-02-13 09:28:06 -0300

    Modificações

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+class App {
+  constructor() {
+    this.server = express();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
