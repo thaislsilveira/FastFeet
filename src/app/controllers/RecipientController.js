61df5dcaca9f0a24976cbe60125473235218d1a4
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-02-14 15:56:58 -0300

    Desafio02-Finalizado

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -57,5 +57,71 @@ class RecipientController {
       cep,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+      street: Yup.string(),
+      number: Yup.number().positive(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { email, oldPassword } = req.body;
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (email !== recipient.email) {
+      const recipientExistis = await Recipient.findOne({
+        where: { email },
+      });
+
+      if (recipientExistis) {
+        return res.status(400).json({ error: 'Recipient already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await recipient.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
 }
 export default new RecipientController();
